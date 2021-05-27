f060b0b2a791f34430cf69a4d253e0893145b14a
Eleonora Lombardo (c) <eleonora.lombardo@avanade.com>
2021-05-27 07:36:24 -0700

    First commit

---
+++ Week2_Esecitazione/Week2_Esecitazione/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week2_Esecitazione
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
